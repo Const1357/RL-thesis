0da8b78d8c47f87e69bb5e96536ad2cd39fe0b0d
Const1357 <theofylaktouc@gmail.com>
2025-07-16 23:45:59 +0300

    experiments (no ALE)

--- generate_plots.py
+++ generate_plots.py
@@ -9,7 +9,7 @@ import re
 
 sns.set_style('darkgrid')
 
-environments = ['CartPole-v1', 'Pendulum-v1', 'ALE/Pong-v5']
+environments = ['CartPole-v1', 'Pendulum-v1']#, 'ALE/Pong-v5']
 # environments = ['CartPole-v1']
 # environments = ['Pendulum-v1']
 
@@ -25,7 +25,7 @@ CONSTANTS = {
         'num_episodes' : 150,
         'max_reward' : 500,
         'min_reward' : 0,
-        'xtick_interval': 10,
+        'xtick_interval': 100000,   # tick every 100k steps
         'ytick_interval': 50,
         'top_offset' : 20,
         'bot_offset' : 1,
@@ -37,7 +37,7 @@ CONSTANTS = {
         'num_episodes' : 500,
         'max_reward' : 0,
         'min_reward' : -1600,
-        'xtick_interval': 50,
+        'xtick_interval': 250000,   # tick every 250k steps
         'ytick_interval': 200,
         'top_offset' : 0,
         'bot_offset' : 0,
@@ -49,7 +49,7 @@ CONSTANTS = {
         'num_episodes' : 800,
         'max_reward' : 21,
         'min_reward' : -21,
-        'xtick_interval': 25,
+        'xtick_interval': 250000,   # tick every 250k steps
         'ytick_interval': 3,
         'top_offset' : 0.5,
         'bot_offset' : 0.2,
@@ -105,11 +105,18 @@ def color_from_name(label: str) -> str:
     return colors_dict[type_from_name(label)]
 
 for env in environments:
-    experiments = os.listdir(f"runs/{env}/pickle/")
+    experiments = os.listdir(f"runs_final/{env}/pickle/")
 
     C = CONSTANTS[env]
 
     all_reward_curves = {
+        'no_mod' : [],
+        'noise' : [],
+        'entropy' : [],
+        'noise_entropy' : [],
+    }
+
+    all_std_ys = {
         'no_mod' : [],
         'noise' : [],
         'entropy' : [],
@@ -140,14 +147,15 @@ for env in environments:
    }
 
     for experiment in experiments:
-        runs = os.listdir(f"runs/{env}/pickle/{experiment}/")
+        runs = os.listdir(f"runs_final/{env}/pickle/{experiment}/")
 
         data = []
         for run in runs:
-            with open(f"runs/{env}/pickle/{experiment}/{run}", 'rb') as f:
+            with open(f"runs_final/{env}/pickle/{experiment}/{run}", 'rb') as f:
                 data.append(pickle.load(f))
 
-        x = np.arange(C['num_episodes'])
+        x = np.arange(0, data[0]['total_steps'], data[0]['log_steps'])
+
         
         # data[0] is the data dictionary for the first run
         # extract data that I will plot, combine it accross runs using numpy (stack and mean across dim)
@@ -156,9 +164,10 @@ for env in environments:
 
         reward_curves = [[s[1] for s in d['reward_curve'] ] for d in data]
 
-        noise_std_curves = [d['noise_stds_curve'] for d in data]
-        stacked_noise_std_curves = np.stack(noise_std_curves).mean(axis=0)
-        norm_stacked_noise_std_curves = (stacked_noise_std_curves-stacked_noise_std_curves.min())/(stacked_noise_std_curves.max()-stacked_noise_std_curves.min())*(C['max_reward'] - C['min_reward']) + C['min_reward']
+        # Noise follows a linear schedule so it does not need to be logged.
+        # noise_std_curves = [d['noise_stds_curve'] for d in data]
+        # stacked_noise_std_curves = np.stack(noise_std_curves).mean(axis=0)
+        # norm_stacked_noise_std_curves = (stacked_noise_std_curves-stacked_noise_std_curves.min())/(stacked_noise_std_curves.max()-stacked_noise_std_curves.min())*(C['max_reward'] - C['min_reward']) + C['min_reward']
 
         entropy_curves = [[s[1] for s in d['entropy_curve'] ] for d in data]
         stacked_entropy_curves = np.stack(entropy_curves).mean(axis=0)
@@ -173,35 +182,39 @@ for env in environments:
 
         fig = plt.figure(figsize=(9,6))
 
-        plt.plot(mean_reward_curve, color='blue', linewidth=1.5, label='Mean Reward')
+        plt.plot(x, mean_reward_curve, color='blue', linewidth=1.5, label='Mean Reward')
         plt.ylim(top=C['max_reward'] + C['top_offset'], bottom=C['min_reward'] + C['bot_offset'])
-        plt.xlim(left=C['left_offset'], right=C['num_episodes']+C['right_offset'])
-        plt.xticks(range(0, C['num_episodes']+1, C['xtick_interval']))
+        plt.xlim(left=C['left_offset'], right=data[0]['total_steps']+C['right_offset'])
+        plt.xticks(range(0, data[0]['total_steps']+1, C['xtick_interval']))
         plt.yticks(range(C['min_reward'],C['max_reward']+1, C['ytick_interval']))
 
         for i,rc in enumerate(reward_curves):
             if i == 0:
-                plt.plot(rc, color='green', alpha=0.2, linewidth=1, label=f"Reward (individual runs)")
+                plt.plot(x, rc, color='green', alpha=0.2, linewidth=1, label=f"Reward (individual runs)")
             else:
-                plt.plot(rc, color='green', alpha=0.2, linewidth=1)
+                plt.plot(x, rc, color='green', alpha=0.2, linewidth=1)
 
-        if not np.isnan(norm_stacked_noise_std_curves).any():
-            plt.plot(norm_stacked_noise_std_curves, color = 'red', linewidth=1, label='Noise std (normalized)')
+        # if not np.isnan(norm_stacked_noise_std_curves).any():
+        #     plt.plot(norm_stacked_noise_std_curves, color = 'red', linewidth=1, label='Noise std (normalized)')
 
-        plt.plot(norm_stacked_entropy_curves, color = 'purple', linewidth=1, label='Entropy (normalized)')
+        plt.plot(x, norm_stacked_entropy_curves, color = 'purple', linewidth=1, label='Entropy (normalized)')
 
 
-        
+        std_fill_y1 = mean_reward_curve-std_reward_curve
+        std_fill_y2 = mean_reward_curve+std_reward_curve
         plt.fill_between(
-            range(len(mean_reward_curve)), mean_reward_curve-std_reward_curve, mean_reward_curve+std_reward_curve, color='blue', alpha=0.1, label='Reward Standard Deviation', edgecolor='none')
+            x, std_fill_y1, std_fill_y2, color='blue', alpha=0.1, label='Reward Standard Deviation', edgecolor='none')
+        all_std_ys[key_from_name(experiment)].append((std_fill_y1, std_fill_y2, experiment))
+        
                 
-        plt.xlabel('Episode')
+        plt.xlabel('Steps')
         plt.ylabel('Reward')
-        plt.legend(loc='lower left')
-        plt.title(f"Rewards over Episodes for {standardize_individual_label(experiment, env)}")
+        plt.suptitle(f"Rewards: {standardize_individual_label(experiment, env)}", fontweight='bold')
+        plt.legend(title=None, loc='upper center', bbox_to_anchor=(0.5, 1.08),
+           ncol=4, frameon=False, fontsize='small')
         plt.tight_layout()
-        savedir_svg = f"runs/{env}/plots/reward_curves/svg/"
-        savedir_png = f"runs/{env}/plots/reward_curves/png/"
+        savedir_svg = f"runs_final/{env}/plots/reward_curves/svg/"
+        savedir_png = f"runs_final/{env}/plots/reward_curves/png/"
         # ensure dirs exist
         os.makedirs(savedir_svg, exist_ok=True)
         os.makedirs(savedir_png, exist_ok=True)
@@ -211,7 +224,71 @@ for env in environments:
         plt.close()
         
 
-        # Plot 2: Entropy, Temperature = f(Episode) - only mean runs, and std around entropy and temperature
+        # Plot 2: Average Policy losses (PPO, auxiliarry losses) for GNN_N
+        if type_from_name(experiment) == 'GNN_N':
+            fig = plt.figure(figsize=(9,6))
+            plt.ylim(top=1.1, bottom=-1.1)
+            plt.yticks(ticks=np.arange(-20, 20+1, 5)/20, labels=[])
+            plt.xlim(left=C['left_offset'], right=data[0]['total_steps']+C['right_offset'])
+            plt.xticks(range(0, data[0]['total_steps']+1, C['xtick_interval']))
+            # plt.yticks(range(C['min_reward'],C['max_reward']+1, C['ytick_interval']))
+
+            selected_indices = np.arange(0, data[0]['total_steps'], data[0]['log_steps']//data[0]['update_steps'])  # log_frequency
+            selected_indices = selected_indices[selected_indices < data[0]['total_steps']//data[0]['update_steps']] # num_episodes
+
+            policy_loss_curves = [np.array(d['policy_loss_curve'])[selected_indices] for d in data]
+            ppo_loss_curves = [np.array(d['ppo_loss_curve'])[selected_indices] for d in data]
+            penalty_loss_curves = [np.array(d['penalty_loss_curve'])[selected_indices] for d in data]
+            margin_loss_curves = [np.array(d['margin_loss_curve'])[selected_indices] for d in data]
+
+            stacked_policy_loss_curve = np.stack(policy_loss_curves)
+            stacked_ppo_loss_curve = np.stack(ppo_loss_curves)
+            stacked_penalty_loss_curve = np.stack(penalty_loss_curves)
+            stacked_margin_loss_curve = np.stack(margin_loss_curves)
+
+            stacked_norm_policy_loss_curve = 0.5*stacked_policy_loss_curve / np.abs(stacked_policy_loss_curve).max(axis=0)
+            stacked_norm_ppo_loss_curve = 0.5*stacked_ppo_loss_curve / np.abs(stacked_ppo_loss_curve).max(axis=0)
+            stacked_norm_penalty_loss_curve = stacked_penalty_loss_curve / np.abs(stacked_penalty_loss_curve).max(axis=0)
+            stacked_norm_margin_loss_curve = stacked_margin_loss_curve / np.abs(stacked_margin_loss_curve).max(axis=0)
+
+            mean_policy_loss_curve = stacked_norm_policy_loss_curve.mean(axis=0)
+            mean_ppo_loss_curve = stacked_norm_ppo_loss_curve.mean(axis=0)
+            mean_penalty_loss_curve = stacked_norm_penalty_loss_curve.mean(axis=0)
+            mean_margin_loss_curve = stacked_norm_margin_loss_curve.mean(axis=0)
+
+            std_policy = stacked_norm_policy_loss_curve.std(axis=0)
+            std_ppo = stacked_norm_ppo_loss_curve.std(axis=0)
+            std_penalty = stacked_norm_penalty_loss_curve.std(axis=0)
+            std_margin = stacked_norm_margin_loss_curve.std(axis=0)
+
+            normalized_mean_reward_curve = 2*(mean_reward_curve - mean_reward_curve.min())/(mean_reward_curve.max()-mean_reward_curve.min()) -1 
+            plt.plot(x, normalized_mean_reward_curve, color='black', label='reward')
+
+            plt.plot(x, mean_ppo_loss_curve, label='PPO Loss',color=colors[0])
+            plt.plot(x, mean_policy_loss_curve, label='Mixed Loss',color=colors[1])
+            plt.plot(x, mean_penalty_loss_curve, label='Penalty Loss',color=colors[2])
+            plt.plot(x, mean_margin_loss_curve, label='Margin_Spread Loss',color=colors[3])
+
+            # plt.fill_between(x, mean_policy_loss_curve-std_policy, mean_policy_loss_curve+std_policy, alpha=0.2, color=colors[0], edgecolor='none')
+            # plt.fill_between(x, mean_ppo_loss_curve-std_ppo, mean_ppo_loss_curve+std_ppo, alpha=0.2, color=colors[1], edgecolor='none')
+            plt.fill_between(x, mean_penalty_loss_curve-std_penalty, mean_penalty_loss_curve+std_penalty, alpha=0.2, color=colors[2], edgecolor='none')
+            plt.fill_between(x, mean_margin_loss_curve-std_margin, mean_margin_loss_curve+std_margin, alpha=0.2, color=colors[3], edgecolor='none')
+
+            plt.plot(x,np.zeros_like(x), color='gray', linestyle='--', linewidth=0.6,)
+            plt.xlabel('Steps')
+            plt.ylabel('Normalized Axis')
+            plt.suptitle(f"Objectives: {standardize_individual_label(experiment, env)}", fontweight='bold')
+            plt.legend(title=None, loc='upper center', bbox_to_anchor=(0.5, 1.08),
+            ncol=5, frameon=False, fontsize='small')
+
+            savedir_svg = f"runs_final/{env}/plots/policy_loss_curves/svg/"
+            savedir_png = f"runs_final/{env}/plots/policy_loss_curves/png/"
+            os.makedirs(savedir_svg, exist_ok=True)
+            os.makedirs(savedir_png, exist_ok=True)
+            fig.savefig(f"{savedir_svg}{experiment}_GNN_N_policy_losses.svg", format='svg')
+            fig.savefig(f"{savedir_png}{experiment}_GNN_N_policy_losses.png", format='png')
+            plt.tight_layout()
+            plt.close()
 
         # Plot 3: Policy Loss & Value loss? useless?
 
@@ -231,27 +308,36 @@ for env in environments:
         to_plot = {
             _type : None for _type in types
         }
+        to_plot_std = {
+            _type : None for _type in types
+        }
+
+        for i, (std_y1, std_y2,label) in enumerate(all_std_ys[mod]):
+            to_plot_std[type_from_name(label)] = (std_y1, std_y2, color_from_name(label), standardize_label(label))
 
         for i,(curve,label) in enumerate(all_reward_curves[mod]):
             to_plot[type_from_name(label)] = (curve, color_from_name(label), standardize_label(label))
 
         for t in types:
             curve = to_plot[t]
+            std_info = to_plot_std[t]
             if curve is not None:
-                plt.plot(curve[0], color=curve[1], label=curve[2])
+                plt.plot(x, curve[0], color=curve[1], label=curve[2])
+                plt.fill_between(x, std_info[0], std_info[1], color = std_info[2], alpha = 0.2, edgecolor = 'none')
 
         plt.ylim(top=C['max_reward'] + C['top_offset'], bottom=C['min_reward'])
-        plt.xlim(left=C['left_offset'], right=C['num_episodes']+C['right_offset'])
-        plt.xticks(range(0, C['num_episodes']+1, C['xtick_interval']))
+        plt.xlim(left=C['left_offset'], right=data[0]['total_steps']+C['right_offset'])
+        plt.xticks(range(0, data[0]['total_steps']+1, C['xtick_interval']))
         plt.yticks(range(C['min_reward'],C['max_reward']+1, C['ytick_interval']))
 
-        plt.title(f'Rewards over Episodes for {standard_environments[env]} ({standardize_mod(mod)})')
-        plt.xlabel('Episode')
+        plt.suptitle(f'Rewards: {standard_environments[env]} ({standardize_mod(mod)})', fontweight='bold')
+        plt.xlabel('Steps')
         plt.ylabel('Reward')
-        plt.legend(loc='upper left')
+        plt.legend(title=None, loc='upper center', bbox_to_anchor=(0.5, 1.08),
+           ncol=4, frameon=False, fontsize='small')
         plt.tight_layout()
-        savedir_svg = f"runs/{env}/plots/reward_curves/svg/all_rewards_{mod}.svg"
-        savedir_png = f"runs/{env}/plots/reward_curves/png/all_rewards_{mod}.png"
+        savedir_svg = f"runs_final/{env}/plots/reward_curves/svg/all_rewards_{mod}.svg"
+        savedir_png = f"runs_final/{env}/plots/reward_curves/png/all_rewards_{mod}.png"
         fig.savefig(savedir_svg, format='svg')
         fig.savefig(savedir_png, format='png')
         # plt.show()
